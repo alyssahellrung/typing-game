b927c9af83406e4d9d455d3e98338d8cf687e086
alyssahellrung <alyssahellrung@gmail.com>
2021-03-15 11:51:19 -0700

    fixed duplicates behind the scenes

--- script.js
+++ script.js
@@ -41,6 +41,9 @@ let score = 0;
 // Init time
 let time = 10;
 
+// Init attemptedWords
+let attemptedWords = [];
+
 // Set difficulty to value in local storage or medium
 let difficulty = localStorage.getItem("difficulty") !== null ? localStorage.getItem("difficulty") : "medium";
 
@@ -58,10 +61,22 @@ function getRandomWord() {
   return words[Math.floor(Math.random() * words.length)];
 }
 
+function checkRandomWord(randomWord) {
+  if(attemptedWords.indexOf(randomWord) == -1) {
+    attemptedWords.push(randomWord);
+    console.log(attemptedWords);
+    return randomWord;
+  } else {
+    randomWord = getRandomWord();
+    checkRandomWord(randomWord);
+  }
+}
+
 // Add word to DOM
 function addWordToDOM() {
   randomWord = getRandomWord();
-  word.innerHTML = randomWord;
+  let goodWord = checkRandomWord(randomWord);
+  word.innerHTML = goodWord;
 }
 
 // Update score
