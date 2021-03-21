9e6c9f70a06ba6d4fe49234a5212e1a2f8a704ee
alyssahellrung <alyssahellrung@gmail.com>
2021-03-20 19:03:10 -0700

    added data file and 100 words

--- script.js
+++ script.js
@@ -9,28 +9,28 @@ const settingsForm = document.getElementById("settings-form");
 const difficultySelect = document.getElementById("difficulty");
 
 // List of words for game
-let words = [
-  "sigh",
-  "tense",
-  "airplane",
-  "ball",
-  "pies",
-  "juice",
-  "warlike",
-  "bad",
-  "north",
-  "dependent",
-  "steer",
-  "silver",
-  "highfalutin",
-  "superficial",
-  "quince",
-  "eight",
-  "feeble",
-  "admit",
-  "drag",
-  "loving",
-];
+// let words = [
+//   "sigh",
+//   "tense",
+//   "airplane",
+//   "ball",
+//   "pies",
+//   "juice",
+//   "warlike",
+//   "bad",
+//   "north",
+//   "dependent",
+//   "steer",
+//   "silver",
+//   "highfalutin",
+//   "superficial",
+//   "quince",
+//   "eight",
+//   "feeble",
+//   "admit",
+//   "drag",
+//   "loving",
+// ];
 
 // Init word
 let randomWord;
